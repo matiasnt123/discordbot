d9ec1a17abc1d8cc1b5cdaf9330cdc9ddce93488
matiasnt <matyoliva@protonmail.com>
2021-01-27 11:41:08 -0300

    added logging to external file

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from dotenv import load_dotenv
 from discord.ext import commands
 from keep_alive import keep_alive
 import discord, sys, os
-# import logging
-# logging.basicConfig(level=logging.INFO)
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 initial_extensions = [
     "cogs.misc", 
